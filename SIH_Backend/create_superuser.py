1ba37dc1101b2169288a7e1f7740a1d6eae305f8
Aman Mishra <amanmishra.nitagartala@gmail.com>
2024-09-20 22:50:51 +0530

    Delete SIH_Backend/create_superuser.py

--- SIH_Backend/create_superuser.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username='<your-username>').exists():
-    User.objects.create_superuser('<your-username>', '<your-email>', '<your-password>')
