ab5ac9fa4c135a5f536c38e017114c83931214d4
Aman Mishra <amanmishra.nitagartala@gmail.com>
2024-09-20 22:52:42 +0530

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -1,6 +1,4 @@
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
-if not User.objects.filter(username='<your-username>').exists():
-    User.objects.create_superuser('<your-username>', '<your-email>', '<your-password>')
+User.objects.create_superuser('admin', 'admin@aman.com', 'aman')
